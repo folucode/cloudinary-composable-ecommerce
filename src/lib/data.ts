d66ed54fb604a48ed2dfa1b68b2fe3169ef520c7
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2024-08-21 11:30:33 +0100

    fix image transformation

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,7 +5,7 @@ export const products: ProductInfo[] = [
     imageUrl:
       'https://cloudinary-app.s3.amazonaws.com/pexels-blue-bird-7210748.jpg',
     transformation:
-      'w_400,h_400,c_scale,q_100,e_auto_brightness,e_auto_color:80',
+      'w_400,h_400,c_auto,g_auto,q_100,e_auto_brightness,e_auto_color:80',
   },
   {
     name: 'Dog Food',
@@ -13,7 +13,7 @@ export const products: ProductInfo[] = [
     imageUrl:
       'https://cloudinary-app.s3.amazonaws.com/pexels-mart-production-8434641.jpg',
     transformation:
-      'w_400,h_400,c_scale,q_100,e_auto_brightness,e_auto_color:80',
+      'w_400,h_400,c_auto,g_auto,q_100,e_auto_brightness,e_auto_color:80',
   },
   {
     name: 'Backpack Carrier',
@@ -21,7 +21,7 @@ export const products: ProductInfo[] = [
     imageUrl:
       'https://images.pexels.com/photos/8358908/pexels-photo-8358908.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     transformation:
-      'w_400,h_400,c_scale,q_100,e_auto_brightness,e_auto_color:80',
+      'w_400,h_400,c_auto,g_auto,q_100,e_auto_brightness,e_auto_color:80',
   },
   {
     name: 'Dog Toy',
@@ -29,7 +29,7 @@ export const products: ProductInfo[] = [
     imageUrl:
       'https://images.pexels.com/photos/20843208/pexels-photo-20843208/free-photo-of-dog-with-toy-on-a-brick-pavement.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     transformation:
-      'w_400,h_400,c_scale,q_100,e_auto_brightness,e_auto_color:80',
+      'w_400,h_400,c_auto,g_auto,q_100,e_auto_brightness,e_auto_color:80',
   },
 ];
 
